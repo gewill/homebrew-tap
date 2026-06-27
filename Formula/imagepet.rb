1d03a012c2669418d352667a9c724808bd26dcc8
Ge Will <531sunlight@gmail.com>
2026-06-27 17:42:52 +0800

    feat(imagepet): update v1.1 formula

--- Formula/imagepet.rb
+++ Formula/imagepet.rb
@@ -1,12 +1,12 @@
 class Imagepet < Formula
   desc "Local-first macOS batch image compressor"
   homepage "https://imagepet.gewill.org/"
-  url "https://github.com/gewill/ImagePet/releases/download/v1.0/imagepet-cli-v1.0-macos-arm64.zip"
-  sha256 "74d5eab54048481ef6e376735fc5952ab0d77eb0c2a757ef057b39875fc0ae94"
+  url "https://github.com/gewill/ImagePet/releases/download/v1.1/imagepet-cli-v1.1-macos-arm64.zip"
+  sha256 "073ad3d82f2d8a266743389bd86797bece8d8ae677901079f52d35b03be45362"
   license "MIT"
 
-  depends_on macos: :ventura
   depends_on arch: :arm64
+  depends_on macos: :ventura
 
   def install
     bin.install "imagepet"
@@ -14,6 +14,6 @@ class Imagepet < Formula
 
   test do
     assert_match "ImagePet", shell_output("#{bin}/imagepet --help")
-    assert_match "1.0", shell_output("#{bin}/imagepet --version")
+    assert_match "1.1", shell_output("#{bin}/imagepet --version")
   end
 end
